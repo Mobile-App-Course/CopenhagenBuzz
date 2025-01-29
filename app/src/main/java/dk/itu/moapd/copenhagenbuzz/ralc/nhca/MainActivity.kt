e1eb49061ce9f17f00bfa72f414ca87bc8688e0f
Nikolaj Carlson <nhca@itu.dk>
2025-01-29 20:58:33 +0100

    Functionality primarily done
    Missing list function for Event Type

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/ralc/nhca/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/ralc/nhca/MainActivity.kt
@@ -2,22 +2,72 @@ package dk.itu.moapd.copenhagenbuzz.ralc.nhca
 
 import android.app.DatePickerDialog
 import android.os.Bundle
+import android .util.Log
+import android.view.Window
+import android .widget . EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx .core.view. WindowCompat
 import com.google.android.material.textfield.TextInputEditText
+import com.google . android . material . floatingactionbutton . FloatingActionButton
 import dk.itu.moapd.copenhagenbuzz.ralc.nhca.databinding.ActivityMainBinding
 import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+
+    companion object {
+        private val TAG = MainActivity::class.qualifiedName
+    }
+
+    // GUI variables
+    private lateinit var eventName: EditText
+    private lateinit var eventLocation: EditText
+    private lateinit var eventDate: EditText
+    private lateinit var eventType: EditText
+    private lateinit var eventDescription: EditText
+    private lateinit var addEventButton: FloatingActionButton
+
+    // Creates an instance of the Event class
+    private val event: Event = Event("", "", "", "", "")
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val editTextEventDate: TextInputEditText = findViewById(R.id.edit_text_event_date)
+        // Linking of UI components
+        eventName = findViewById(R.id.edit_text_event_name)
+        eventLocation = findViewById(R.id.edit_text_event_location)
+        eventDate = findViewById(R.id.edit_text_event_date)
+        eventType = findViewById(R.id.edit_text_event_type)
+        eventDescription = findViewById(R.id.edit_text_event_description)
+        addEventButton = findViewById(R.id.floating_button_event_add)
+
+        // Listener for user interaction in the `Add Event` button.
+        addEventButton.setOnClickListener {
+            // Only execute the following code when the user fills all `EditText`.
+            if (eventName.text.toString().isNotEmpty() &&
+                eventLocation.text.toString().isNotEmpty()) {
+
+                // Update the object attributes.
+                event.setEventName(eventName.text.toString().trim())
+                event.setEventLocation(eventLocation.text.toString().trim())
+                event.setEventDate(eventDate.text.toString().trim())
+                event.setEventType(eventType.text.toString().trim())
+                event.setEventDescription(eventDescription.text.toString().trim())
+                // TODO: Implement the missing code here.
+
+                // Write in the `Logcat` system.
+                showMessage()
+            }
+        }
+
         // Set a OnClickListener to show the calendar when clicked
-        editTextEventDate.setOnClickListener {
-            showCalendar(editTextEventDate)
+        eventDate.setOnClickListener {
+            showCalendar(eventDate as TextInputEditText)
         }
     }
 
@@ -40,5 +90,9 @@ class MainActivity : AppCompatActivity() {
             calYear, calMonth, calDay
         )
         calendarPicker.show()
+    }
+
+    private fun showMessage() {
+        Log.d(TAG, event.toString())
     }
 }
