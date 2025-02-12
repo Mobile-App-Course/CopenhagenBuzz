d489e0ae79cec2311ec8913d7fcfdf0d7e304626
Nikolaj Carlson <nhca@itu.dk>
2025-02-12 17:32:34 +0100

    3.3 done
    Co-Authored-By: Rasmus Alexander Christiansen <54313567+RasmusAChr@users.noreply.github.com>

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/ralc/nhca/View/LoginActivity.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/ralc/nhca/View/LoginActivity.kt
@@ -1,21 +1,42 @@
 package dk.itu.moapd.copenhagenbuzz.ralc.nhca.View
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import dk.itu.moapd.copenhagenbuzz.ralc.nhca.R
+import dk.itu.moapd.copenhagenbuzz.ralc.nhca.databinding.ActivityLoginBinding
+import dk.itu.moapd.copenhagenbuzz.ralc.nhca.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+
+        // Inflate the layout for this activity
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.materialButtonLogin.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.materialButtonGuest.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
