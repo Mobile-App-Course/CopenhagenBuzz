b330725b0c5a245273bb7d14eb5f12d01d3623bb
RasmusAC <ralc@itu.dk>
2025-02-20 21:42:41 +0100

    Update MainViewModel.kt

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/ralc/nhca/ViewModel/MainViewModel.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/ralc/nhca/ViewModel/MainViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 class MainViewModel : ViewModel() {
     var eventName: String? = null
     var eventLocation: String? = null
+    var eventPhotoURL: String? = null
     var eventDate: String? = null
     var eventType: String? = null
     var eventDescription: String? = null
