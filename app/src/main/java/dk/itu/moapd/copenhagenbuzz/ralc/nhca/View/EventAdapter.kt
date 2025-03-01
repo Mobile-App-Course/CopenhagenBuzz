f5be40d24b921f0f719f9dbff634b1b4f626c299
RasmusAChr <ralc@itu.dk>
2025-03-01 20:44:49 +0100

    Using viewbinding for EventAdapter

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/ralc/nhca/View/EventAdapter.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/ralc/nhca/View/EventAdapter.kt
@@ -1,86 +1,55 @@
 package dk.itu.moapd.copenhagenbuzz.ralc.nhca.View
 
-import android.content.Context
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.TextView
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.button.`MaterialButton$InspectionCompanion`
-import com.google.android.material.snackbar.Snackbar
-import dk.itu.moapd.copenhagenbuzz.ralc.nhca.Model.Event
-import dk.itu.moapd.copenhagenbuzz.ralc.nhca.R
-import com.squareup.picasso.Picasso
+ import android.content.Context
+ import android.view.LayoutInflater
+ import android.view.View
+ import android.view.ViewGroup
+ import android.widget.ArrayAdapter
+ import com.google.android.material.snackbar.Snackbar
+ import com.squareup.picasso.Picasso
+ import dk.itu.moapd.copenhagenbuzz.ralc.nhca.Model.Event
+ import dk.itu.moapd.copenhagenbuzz.ralc.nhca.R
+ import dk.itu.moapd.copenhagenbuzz.ralc.nhca.databinding.EventRowItemBinding
 
-/**
- * An adapter that provides views for displaying Event data in a ListView.
- *
- * @param context The context in which the adapter is running.
- * @param resource The resource ID for a layout file containing a layout to use when instantiating views.
- * @param data The list of Event objects to represent in the ListView.
- */
 class EventAdapter(private val context: Context, private var resource: Int, data: List<Event>) :
-    ArrayAdapter<Event>(context, R.layout.event_row_item, data) {
+     ArrayAdapter<Event>(context, R.layout.event_row_item, data) {
 
-    /**
-     * A ViewHolder class that holds references to the views for each data item.
-     *
-     * @constructor Creates a ViewHolder and initializes the views.
-     * @param view The view containing the views to be initialized.
-     */
-    private class ViewHolder(view: View) {
-        val eventName: TextView = view.findViewById(R.id.event_name_text_view)
-        val eventPhoto: ImageView = view.findViewById(R.id.event_photo_image_view)
-        val eventSubtitle: TextView = view.findViewById(R.id.event_subtitle_text_view)
-        val eventDescription: TextView = view.findViewById(R.id.event_description_text_view)
-        val favoriteButton: MaterialButton = view.findViewById(R.id.button_favorite)
-        val shareButton: MaterialButton = view.findViewById(R.id.button_share)
-    }
+     private class ViewHolder(val binding: EventRowItemBinding)
 
-    /**
-     * Returns a view for the specified position in the data set.
-     *
-     * @param position The position of the item within the adapter's data set.
-     * @param convertView The old view to reuse, if possible.
-     * @param parent The parent that this view will eventually be attached to.
-     * @return A view corresponding to the data at the specified position.
-     */
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view = convertView ?: LayoutInflater.from(context).inflate(resource, parent, false)
-        val viewHolder = (view.tag as? ViewHolder) ?: ViewHolder(view)
+     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+         val binding: EventRowItemBinding
+         val viewHolder: ViewHolder
 
-        getItem(position)?.let { event ->
-            populateViewHolder(viewHolder, event)
-        }
+         if (convertView == null) {
+             binding = EventRowItemBinding.inflate(LayoutInflater.from(context), parent, false)
+             viewHolder = ViewHolder(binding)
+             binding.root.tag = viewHolder
+         } else {
+             binding = (convertView.tag as ViewHolder).binding
+             viewHolder = convertView.tag as ViewHolder
+         }
 
-        return view
-    }
+         getItem(position)?.let { event ->
+             populateViewHolder(viewHolder, event)
+         }
 
-    /**
-     * Populates the ViewHolder with data from the given Event object.
-     *
-     * @param viewHolder The ViewHolder containing the views to be populated.
-     * @param event The Event object containing the data to populate the views.
-     */
-    private fun populateViewHolder(viewHolder: ViewHolder, event: Event) {
-        with(viewHolder) {
-            Picasso.get().load(event.eventPhotoURL).into(eventPhoto)
-            eventName.text = event.eventName
-            eventSubtitle.text = context.getString(R.string.event_subtitle, event.eventDate, event.eventLocation, event.eventType)
-            eventDescription.text = event.eventDescription
+         return binding.root
+     }
 
-            // Favorite button listener
-            favoriteButton.setOnClickListener { view ->
-                Snackbar.make(view, "Event Favorite!", Snackbar.LENGTH_SHORT).show()
-            }
+     private fun populateViewHolder(viewHolder: ViewHolder, event: Event) {
+         with(viewHolder.binding) {
+             Picasso.get().load(event.eventPhotoURL).into(eventPhotoImageView)
+             eventNameTextView.text = event.eventName
+             eventSubtitleTextView.text = context.getString(R.string.event_subtitle, event.eventDate, event.eventLocation, event.eventType)
+             eventDescriptionTextView.text = event.eventDescription
 
-            // Share button listener
-            shareButton.setOnClickListener { view ->
-                Snackbar.make(view, "Event Shared!", Snackbar.LENGTH_SHORT).show()
-            }
+             buttonFavorite.setOnClickListener { view ->
+                 Snackbar.make(view, "Event Favorite!", Snackbar.LENGTH_SHORT).show()
+             }
 
-        }
-    }
-}
+             buttonShare.setOnClickListener { view ->
+                 Snackbar.make(view, "Event Shared!", Snackbar.LENGTH_SHORT).show()
+             }
+         }
+     }
+ }
