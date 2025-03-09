27bcb0fba99095e8b9205462105e7c855575572f
Nikolaj Carlson <70134368+Nikolaj787@users.noreply.github.com>
2025-03-09 13:22:08 +0100

    Implemented RecyclerView
    But need to doublecheck favorites, if the ones that are favorited are actually favorited

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/ralc/nhca/View/FavoritesFragment.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/ralc/nhca/View/FavoritesFragment.kt
@@ -5,12 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import dk.itu.moapd.copenhagenbuzz.ralc.nhca.R
+import dk.itu.moapd.copenhagenbuzz.ralc.nhca.ViewModel.DataViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,17 +18,8 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class FavoritesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val dataViewModel: DataViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +29,20 @@ class FavoritesFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_favorites, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.favorites_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        dataViewModel.favoriteEvents.observe(viewLifecycleOwner) { favoriteEvents ->
+            val adapter = FavoriteAdapter(requireContext(), favoriteEvents)
+            recyclerView.adapter = adapter
+        }
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FavoritesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FavoritesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = FavoritesFragment()
     }
 }
