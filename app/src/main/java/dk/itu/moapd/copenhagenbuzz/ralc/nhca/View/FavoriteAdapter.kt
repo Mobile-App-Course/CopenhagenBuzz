3f2e3e263fb8d48e289735ca19768236e274e2bf
Nikolaj Carlson <70134368+Nikolaj787@users.noreply.github.com>
2025-03-24 23:09:42 +0100

    Favorites can now be loaded from Firebase through the database
    The user can't favorite an event yet, but the two developers have some dummy data, to see it being loaded.
    
    The database has then also been updated to reflect these changes, and the rules have been adjusted to make sure they are correct for the app

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/ralc/nhca/View/FavoriteAdapter.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/ralc/nhca/View/FavoriteAdapter.kt
@@ -4,34 +4,63 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.database
 import com.squareup.picasso.Picasso
 import dk.itu.moapd.copenhagenbuzz.ralc.nhca.Model.Event
 import dk.itu.moapd.copenhagenbuzz.ralc.nhca.databinding.FavoriteRowItemBinding
+import io.github.cdimascio.dotenv.dotenv
 
 class FavoriteAdapter(
+    options: FirebaseRecyclerOptions<Event>,
     private val context: Context,
-    private val favoriteEvents: List<Event>
-) : RecyclerView.Adapter<FavoriteAdapter.ViewHolder>() {
+) : FirebaseRecyclerAdapter<Event, FavoriteAdapter.ViewHolder>(options) {
 
     inner class ViewHolder(val binding: FavoriteRowItemBinding): RecyclerView.ViewHolder(binding.root)
+
+    companion object{
+        fun create(context: Context): FavoriteAdapter {
+            val currentUser = FirebaseAuth.getInstance().currentUser
+            val userId = currentUser?.uid ?: ""
+
+            // Load environment variables
+            val dotenv = dotenv {
+                directory = "./assets"
+                filename = "env"
+            }
+
+            // Create query for this user's favorites
+            val databaseRef = Firebase.database(dotenv["DATABASE_URL"])
+                .reference.child("favorites").child(userId)
+
+            // Configure FirebaseRecyclerOptions
+            val options = FirebaseRecyclerOptions.Builder<Event>()
+                .setQuery(databaseRef, Event::class.java)
+                .build()
+
+            return FavoriteAdapter(options, context)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = FavoriteRowItemBinding.inflate(LayoutInflater.from(context), parent, false)
         return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val event = favoriteEvents[position]
+    override fun onBindViewHolder(holder: ViewHolder, position: Int, model: Event) {
         with(holder.binding) {
-            Picasso.get().load(event.eventPhotoURL).into(eventPhotoImageView)
-            eventNameTextView.text = event.eventName
-            eventTypeTextView.text = event.eventType
+            eventNameTextView.text = model.eventName
+            eventTypeTextView.text = model.eventType
 
-
+            // Picasso used to load image
+            Picasso.get()
+                .load(model.eventPhotoURL)
+                .fit()
+                .centerCrop()
+                .into(eventPhotoImageView)
         }
-    }
-
-    override fun getItemCount(): Int {
-        return favoriteEvents.size
     }
 }
--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/ralc/nhca/View/FavoritesFragment.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/ralc/nhca/View/FavoritesFragment.kt
@@ -14,14 +14,20 @@ import dk.itu.moapd.copenhagenbuzz.ralc.nhca.ViewModel.DataViewModel
 
 
 /**
- * A simple [Fragment] subclass.
- * Use the [FavoritesFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * A simple [Fragment] subclass to display favorite events in a RecyclerView.
+ * Use the [FavoritesFragment.newInstance] factory method to create an instance of this fragment.
  */
 class FavoritesFragment : Fragment() {
 
-    private val dataViewModel: DataViewModel by activityViewModels()
+    private lateinit var adapter: FavoriteAdapter
 
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     * @param inflater The LayoutInflater object that can be used to inflate any views in the fragment.
+     * @param container If non-null, this is the parent view that the fragment's UI should be attached to.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state as given here.
+     * @return Return the View for the fragment's UI, or null.
+     */
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,20 +36,46 @@ class FavoritesFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_favorites, container, false)
     }
 
+    /**
+     * Called immediately after onCreateView() has returned, but before any saved state has been restored in to the view.
+     * @param view The View returned by onCreateView().
+     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state as given here.
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val recyclerView: RecyclerView = view.findViewById(R.id.favorites_recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-        dataViewModel.favoriteEvents.observe(viewLifecycleOwner) { favoriteEvents ->
-            val adapter = FavoriteAdapter(requireContext(), favoriteEvents)
-            recyclerView.adapter = adapter
-        }
+        // Creates and set the Firebase adapter
+        adapter = FavoriteAdapter.create(requireContext())
+        recyclerView.adapter = adapter
     }
 
     companion object {
+        /**
+         * Factory method to create a new instance of this fragment.
+         * @return A new instance of fragment FavoritesFragment.
+         */
         @JvmStatic
         fun newInstance() = FavoritesFragment()
+    }
+
+    /**
+     * Called when the Fragment is visible to the user and actively running.
+     * Starts listening for data changes in the Firebase adapter.
+     */
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    /**
+     * Called when the Fragment is no longer started.
+     * Stops listening for data changes in the Firebase adapter.
+     */
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
     }
 }
